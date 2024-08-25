f406e132d721e6547a53cfd8c4eb8131f032a6d3
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2024-08-25 12:23:59 -0300

    address pylint results

--- notepad.py
+++ notepad.py
@@ -1,7 +1,3 @@
-# display scoreboard with clear screen
-# allow [h]elp for accessing game rules
-# display drawn in the same manner as winners
-
 """Rock Paper Scissors Spock Lizard"""
 
 from collections import namedtuple
@@ -26,26 +22,34 @@ CHOICES = {
 }
 
 def main():
-    """
-    Program entry point
-    """
+    """Program entry point"""
+    play_best_of_five()
+    while keep_going():
+        play_best_of_five()
+
+def play_best_of_five():
+    """Play a single game of best of five"""
     rounds = 0
     tally = {"player": 0, "computer": 0}
     while tally["player"] < 3 and tally["computer"] < 3:
         rounds += 1
         user_choice = get_user_choice()
+        if user_choice.choice == "help":
+            display_help()
+            continue
         computer_choice = get_computer_choice()
-        
+
         winner = decide_winner(user_choice, computer_choice)
         display_winner(winner)
-        
+
         tally = tally_result(winner, tally)
         display_scoreboard(tally, rounds)
 
 Winner = namedtuple("Winner", ["play", "action"])
-Play = namedtuple("Play", ["username", "choice"]) 
+Play = namedtuple("Play", ["username", "choice"])
 
 def tally_result(winner, tally):
+    """Determine winner and tally results for scoreboard"""
     if winner and winner.play.username == "player":
         tally["player"] += 1
     if winner and winner.play.username == "computer":
@@ -53,88 +57,128 @@ def tally_result(winner, tally):
     return tally
 
 def get_user_choice() -> Play:
-    message = f"""Choose one of:
+    """Get a valid user choice"""
+    message = """Choose one of (or [h]elp for game rules):
 - [r]ock
 - [p]aper
 - [sc]issors
 - [l]izard
 - [sp]ock"""
     prompt(message)
-    choice = input()
+    choice = input().strip().lower()
     while is_user_choice_invalid(choice):
         prompt(f"{choice!r} is not valid. {message}")
-        choice = input()
-    return Play("player", CHOICES[choice])
+        choice = input().strip().lower()
+    return Play("player", CHOICES.get(choice, "help"))
 
 def is_user_choice_invalid(choice: str):
-    return choice not in CHOICES
+    """Returns True if user choice is invalid.
+    
+    Allows user asking for help.
+    """
+    return choice not in CHOICES and choice not in ["help", "h"]
 
 def get_computer_choice() -> Play:
+    """Select a random play for computer"""
     choice = random.choice(list(set(CHOICES.values())))
     return Play("computer", choice)
 
-def decide_winner(playA, playB) -> Play:
+def decide_winner(play_player, play_computer) -> Play:
     """
-    Given a pair of Plays (username, choice), return the tuple of the winner player or None in case of draw.
+    Given a pair of Plays returns Winner
     """
-    game_result = get_result_message({playA.choice, playB.choice})
-    if playA.choice == playB.choice:
-        return None
-    if playA.choice == "rock" and playB.choice in ("scissors", "lizard"):
-        return Winner(playA, game_result)
-    if playA.choice == "scissors" and playB.choice in ("paper", "lizard"):
-        return Winner(playA, game_result)
-    if playA.choice == "paper" and playB.choice in ("rock", "spock"):
-        return Winner(playA, game_result)
-    if playA.choice == "spock" and playB.choice in ("scissors", "rock"):
-        return Winner(playA, game_result)
-    if playA.choice == "lizard" and playB.choice in ("spock", "paper"):
-        return Winner(playA, game_result)
+    game_result = get_result_message({play_player.choice, play_computer.choice})
+    if play_player.choice == play_computer.choice:
+        result = None
+    if play_player.choice == "rock" and play_computer.choice in ("scissors", "lizard"):
+        result = Winner(play_player, game_result)
+    if play_player.choice == "scissors" and play_computer.choice in ("paper", "lizard"):
+        result = Winner(play_player, game_result)
+    if play_player.choice == "paper" and play_computer.choice in ("rock", "spock"):
+        result = Winner(play_player, game_result)
+    if play_player.choice == "spock" and play_computer.choice in ("scissors", "rock"):
+        result = Winner(play_player, game_result)
+    if play_player.choice == "lizard" and play_computer.choice in ("spock", "paper"):
+        result = Winner(play_player, game_result)
 
-    return Winner(playB, game_result)
+    result = Winner(play_computer, game_result)
+    return result
 
 def display_winner(winner: Winner):
+    """Display winner and winner action"""
     if winner:
-        prompt("%s won! %s" % (winner.play.username.capitalize(), winner.action))
+        winner_username = winner.play.username.capitalize()
+        prompt(f"{winner_username} won! {winner.action}")
     else:
         prompt("Drawn!")
-        
+
     print("...")
     time.sleep(2)
-    os.system("clear")
+    clear()
 
 def display_scoreboard(tally, rounds):
+    """Display scoreboard with total rounds"""
     horizontal_line = f"+-{'-'*26}-+"
     print(horizontal_line)
     print(f"|  Best of five ({rounds:02} rounds)  |")
-    print("| Player (%d) vs Computer (%d) |" % (tally["player"], tally["computer"]))
+    player_score = tally["player"]
+    computer_score = tally["computer"]
+    print(f"| Player ({player_score}) vs Computer ({computer_score}) |")
     print(horizontal_line)
 
-def get_result_message(choices):
+def display_help():
+    """Display help"""
+    clear()
+    prompt("""Scissors cuts paper. Paper covers rock. Rock crushes lizard.
+Lizard poisons Spock. Spock smashes scissors. Scissors decapitates lizard.
+Lizard eats paper. Paper disproves Spock. Spock vaporizes rock. 
+Rock crushes scissors.""")
 
- if choices == {"lizard", "spock"}:
-    return "Lizard poisons Spock."
- if choices == {"lizard", "paper"}:
-    return "Lizard eats Paper."
- if choices == {"spock", "scissors"}:
-    return "Spock smashes Scissors."
- if choices == {"spock", "rock"}:
-    return "Spock vaporizes Rock."
- if choices == {"paper", "rock"}:
-    return "Paper covers Rock."
- if choices == {"paper", "spock"}:
-    return "Paper disproves Spock."
- if choices == {"scissors", "lizard"}:
-    return "Scissors decapitates Lizard."
- if choices == {"scissors", "paper"}:
-    return "Scissors cuts Paper."
- if choices == {"rock", "scissors"}:
-    return "Rock crushes Scissors."
- if choices == {"rock", "lizard"}:
-    return "Rock crushes Lizard."
+def get_result_message(choices):
+    """Retrieve action performed by winning player"""
+
+    result = None
+
+    if choices == {"lizard", "spock"}:
+        result = "Lizard poisons Spock."
+    if choices == {"lizard", "paper"}:
+        result = "Lizard eats Paper."
+    if choices == {"spock", "scissors"}:
+        result = "Spock smashes Scissors."
+    if choices == {"spock", "rock"}:
+        result = "Spock vaporizes Rock."
+    if choices == {"paper", "rock"}:
+        result = "Paper covers Rock."
+    if choices == {"paper", "spock"}:
+        result = "Paper disproves Spock."
+    if choices == {"scissors", "lizard"}:
+        result = "Scissors decapitates Lizard."
+    if choices == {"scissors", "paper"}:
+        result = "Scissors cuts Paper."
+    if choices == {"rock", "scissors"}:
+        result = "Rock crushes Scissors."
+    if choices == {"rock", "lizard"}:
+        result = "Rock crushes Lizard."
+
+    return result
 
 def prompt(message):
+    """Format message for printing to user"""
     print(f"==> {message}")
+
+def keep_going():
+    """Continue playing?"""
+    prompt("Play again? Yes/No")
+    if input().strip().lower() in ["y", "yes"]:
+        return True
+    return False
+
+def clear():
+    """Clear terminal screen"""
+    if os.name == "posix":
+        _ = os.system("clear")  # mac/linux
+    else:
+        _ = os.system("cls")  # windows
 
 if __name__ == "__main__":
     main()
--- test_notepad.py
+++ test_notepad.py
@@ -1,7 +1,3 @@
-from notepad import decide_winner, Play, display_winner
+from notepad import is_user_choice_invalid
 
-winner = decide_winner(Play(username='player', choice='spock'),
-              Play(username='computer', choice='spock'))
-
-
-display_winner(winner, "foo")
+is_user_choice_invalid("h")
